050a52261f077181db81688dbf89da17c599a9d8
Alennikov Boris <70971570+AlenniBoris@users.noreply.github.com>
2023-02-05 21:45:59 +0300

    commit

---
+++ C#/LearningProject/LearningProject/Program.cs
@@ -0,0 +1,40 @@
+﻿using System.Runtime.CompilerServices;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+
+        //string name = "john";
+        //console.writeline(name);
+        //console.write(name + " = ");
+        //console.write(name);
+
+        //Console.WriteLine();
+
+        //int x, y, z;
+        //x = y = z = 60;
+        //console.writeline(x);
+        //console.writeline(y);
+        //console.writeline(z);
+        //console.writeline(x + y + z);
+
+        //long myNum = 15000000000L;
+        //Console.WriteLine(myNum);
+
+        //float f1 = 35e3F;
+        //double d1 = 12E4D;
+        //Console.WriteLine(f1);
+        //Console.WriteLine(d1);
+
+        //double myDouble = 6.78;
+        //int myInt = (int)myDouble;
+        //Console.WriteLine(myDouble);
+        //Console.WriteLine(myInt);
+
+        //fuck u comments in VS
+
+
+    }
+}
